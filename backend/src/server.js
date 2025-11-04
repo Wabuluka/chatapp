aecd1e33d53c9a477728a2c74b631644760819a6
Davies Wabuluka <dwabuluka@gmail.com>
2025-11-04 09:47:38 -0600

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 app.use(express.json());
-
+const __dirname = path.resolve();
 dotenv.config();
+
+console.log(__dirname);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // Serve index.html for all other routes (client-side routing)
+  app.get(":all", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../dist/index.html"));
+  });
+}
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port: `, PORT));
