e76a4589cc0f7707fa763a7f55153ba5ded283f1
Davies Wabuluka <dwabuluka@gmail.com>
2025-11-06 13:49:35 -0600

    sign up page done!

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,4 +1,33 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
 export const useAuthStore = create((set) => ({
-  authUser: { name: "John", id: 123, age: 24 },
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in authCheck", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    try {
+      set({ isSigningUp: true });
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    } catch (error) {
+      toast.error(error.response.error.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
 }));
