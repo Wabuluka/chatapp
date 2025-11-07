fc4cb310d7aba15976efb0d19cf08f30e2632a40
Davies Wabuluka <dwabuluka@gmail.com>
2025-11-06 18:42:48 -0600

    chat pages

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-import { useAuthStore } from "../store/useAuthStore";
+
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
+import ProfileHeader from "../components/ProfileHeader";
+import ActiveTabSwitch from "../components/ActiveTabSwitch";
+import ChatList from "../components/ChatList";
+import ContactList from "../components/ContactList";
+import { useChatStore } from "../store/useChatStore";
+import ChatContainer from "../components/ChatContainer";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 
 function ChatPage() {
-  const { logout } = useAuthStore();
+  const { activeTab, selectedUser } = useChatStore();
 
   return (
-    <div className="text-white z-10">
-      <button onClick={logout} className="btn">
-        Sign Out
-      </button>
+    <div className="relative w-full max-w-6xl h-[800px]">
+      <BorderAnimatedContainer>
+        {/* left */}
+        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatList /> : <ContactList />}
+          </div>
+        </div>
+        {/* right side */}
+        <div className=" flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm ">
+          {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+        </div>
+      </BorderAnimatedContainer>
     </div>
   );
 }
